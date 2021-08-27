3161ec03f0665158c2a9ef3fed5aa4bb37adeecf
EthanLo01 <89072949+EthanLo01@users.noreply.github.com>
2021-08-28 02:17:43 +0800

    Add files via upload

---
+++ Dp/70_Climbing_Stairs.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 28 01:45:31 2021
+
+@author: user
+"""
+# God:
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        result = 0;
+        pre = 1;
+        next = 2;
+        i = 3
+        while i <= n:
+
+
+            result = pre + next
+            pre = next
+            next = result
+            i +=1
+            
+
+        return result;
