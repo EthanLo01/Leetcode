d4cf444793088d171a56b7f0f8279e4694e7cad0
EthanLo01 <89072949+EthanLo01@users.noreply.github.com>
2021-09-01 22:31:54 +0800

    Add files via upload

---
+++ Matrix/54. Spiral Matrix.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 20:17:55 2021
+
+@author: user
+"""
+# https://leetcode.com/problems/spiral-matrix/solution/
+class Solution:
+    def spiralOrder(self, matrix):
+        result = []
+        rows, columns = len(matrix), len(matrix[0])
+        up = left = 0
+        right = columns - 1
+        down = rows - 1
+
+        while len(result) < rows * columns:
+            # Traverse from left to right.
+            for col in range(left, right + 1):
+                result.append(matrix[up][col])
+
+            # Traverse downwards.
+            for row in range(up + 1, down + 1):
+                result.append(matrix[row][right])
+
+            # Make sure we are now on a different row.
+            if up != down:
+                # Traverse from right to left.
+                for col in range(right - 1, left - 1, -1):
+                    result.append(matrix[down][col])
+
+            # Make sure we are now on a different column.
+            if left != right:
+                # Traverse upwards.
+                for row in range(down - 1, up, -1):
+                    result.append(matrix[row][left])
+
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+
+        return result
